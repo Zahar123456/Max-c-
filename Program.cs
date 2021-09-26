714f3a530102634e38ea06df9e79949e1b943773
Zahar123456 <81895264+Zahar123456@users.noreply.github.com>
2021-09-26 09:35:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string : ");
+            string userInput = Console.ReadLine();
+            RemoveLetters(userInput);
+          
+        }
+
+        static void RemoveLetters(string inputString)
+        {
+            List<String> wrongLetters = new List<String>();
+            wrongLetters.Add("o");
+            wrongLetters.Add("i");
+            wrongLetters.Add("u");
+            wrongLetters.Add("a");
+            wrongLetters.Add("e");
+            
+            wrongLetters.Add("O");
+            wrongLetters.Add("I");
+            wrongLetters.Add("U");
+            wrongLetters.Add("A");
+            wrongLetters.Add("E");
+
+            string result = "";
+            for(int i = 0; i < inputString.Length; i++ )
+            {
+                if (!wrongLetters.Contains(inputString[i].ToString()))
+                {
+                    result = result + inputString[i];
+                }
+            }
+            Console.WriteLine("Result:" + result);
+        } 
+        
+            
+       
+                           
+        
+    }
+
+}
