0a9ae687d76a39da25c3e2de0f5bfdd9ae1032ab
Zahar123456 <81895264+Zahar123456@users.noreply.github.com>
2021-10-03 07:07:04 +0300

    Add files via upload

---
+++ Stepen.cs
@@ -0,0 +1,12 @@
+
+using System;
+class Task
+{
+    static void Main() 
+    {   
+       
+        int x = 5;
+        int stepen = 4;
+        Console.WriteLine("5^4 = " + Math.Pow(x, stepen));
+    }
+}
